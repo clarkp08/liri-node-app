a99594776ca0269ac01a196ef777237351e6fb24
Phil Clark <clarkp08@gmail.com>
2017-08-29 13:32:43 -0400

    update

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'EmIMfa69d4jV6QXmRRduOFsP2',
+  consumer_secret: 'EecKQlkVGqAZUQQUOENrKxMD3soNImKZTE9C0KZzd4oqSTiKZz',
+  access_token_key: '902571016190353412-9dGerUpS16FdvrWpZjH4UtaUTmrasmx',
+  access_token_secret: 'fO9V9XWBTTxhimQIz1zDvnrrbgHvm5JUQSp1kvQtEpgNT',
+}
+
+// exports.spotifyKeys = {
+	
+// }
